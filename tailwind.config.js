6ecb87aa1f372b469d69df0172990b146af92b67
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-06-20 15:15:23 +0900

    ✅ TailwindCSS 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
