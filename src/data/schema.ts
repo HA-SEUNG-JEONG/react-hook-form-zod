9da8f210b16d2001300b7eb3782fac9af73f9f28
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2023-06-21 17:09:22 +0900

    👔 비밀번호 검증 로직 추가

--- src/data/schema.ts
+++ src/data/schema.ts
@@ -10,10 +10,18 @@ export const schema = z
       .string()
       .min(1, { message: "1글자 이상은 채워주세요." })
       .email({ message: "유효한 이메일을 입력하세요" }),
-    password: z.string().min(6, "6글자 이상으로 해주세요."),
+    password: z
+      .string()
+      .min(6, "비밀번호는 최소 6글자 이상이어야 합니다.")
+      .refine((val) => {
+        const strongRegex =
+          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+        return strongRegex.test(val);
+      }, "비밀번호는 영문 대문자, 영문 소문자, 숫자, 특수문자를 포함해야 합니다."),
+
     confirmPassword: z
       .string()
-      .min(1, { message: "비밀번호 확인란은 필수항목입니다." }),
+      .min(6, { message: "비밀번호 확인란은 필수항목입니다." }),
   })
   .refine((data) => data.password === data.confirmPassword, {
     path: ["confirmPassword"],
